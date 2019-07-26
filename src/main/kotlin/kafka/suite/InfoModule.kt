c2d55f7f075f19bee7a28d1578658b05c04a8240
asubb <a.a.subb@gmail.com>
2019-07-26 19:16:00 -0400

    [basic info command]

--- src/main/kotlin/kafka/suite/InfoModule.kt
+++ src/main/kotlin/kafka/suite/InfoModule.kt
@@ -7,24 +7,32 @@ import org.apache.commons.cli.Options
 
 class InfoModule : RunnableModule {
 
-    private val t = Option("t", "topics", true, "Comma-separated list of topics to include, if not specified all topics will be included.")
+    private val t = Option("t", "topics", true, "Comma-separated list of topics to get info for")
+    private val a = Option("a", "all-topics", false, "Get info for all topics")
+    private val b = Option("b", "brokers", false, "Get info about brokers")
 
     override fun module(): Module = Module.INFO
 
-    override fun getOptions(): Options = Options().of(t)
+    override fun getOptions(): Options = Options().of(t, a, b)
 
     override fun getDescription(): String = "Gets info about cluster"
 
     override fun run(cli: CommandLine, kafkaAdminClient: KafkaAdminClient, dryRun: Boolean, waitToFinish: Boolean) {
-        val topics = cli.get(t, emptySet()) { it.first().toString().split(",").toSet() }
+        cli.ifHas(t) {
+            val topics = cli.get(t, emptySet()) { it.first().toString().split(",").toSet() }
+            val assignment = kafkaAdminClient.currentAssignment(topics)
+            println("Assignment: $assignment")
+        }
 
-        val brokers = kafkaAdminClient.brokers()
-        val assignment = kafkaAdminClient.currentAssignment(topics)
+        cli.ifHas(a) {
+            val assignment = kafkaAdminClient.currentAssignment()
+            println("Assignment: $assignment")
+        }
 
-        println("""
-            Brokers [${brokers.size}]: $brokers
-            
-            Assignment: $assignment
-        """.trimIndent())
+        cli.ifHas(b) {
+            val brokers = kafkaAdminClient.brokers()
+
+            println("Brokers [${brokers.size}]: $brokers")
+        }
     }
 }
